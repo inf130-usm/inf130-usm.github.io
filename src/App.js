d06e914635caac7e495d55820214d99f8ed8361d
Pedro Arriagada <parriaga@alumnos.inf.utfsm.cl>
2022-08-25 16:32:05 -0400

    Google A?

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from './components/HeaderFooter/Header';
 import Body from './components/Body/Body';
 import Footer from './components/HeaderFooter/Footer';
 import './App.css';
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-2E4BMVMC2T"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
