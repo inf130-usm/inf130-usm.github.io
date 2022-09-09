4b2b7b476fd4b74e69a0697a3d3048653a55bb49
Pedro Arriagada <parriaga@alumnos.inf.utfsm.cl>
2022-09-09 02:51:35 -0300

    Actualizado React-GA

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Header from './components/HeaderFooter/Header';
 import Body from './components/Body/Body';
 import Footer from './components/HeaderFooter/Footer';
 import './App.css';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 const TRACKING_ID = "G-2E4BMVMC2T"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send(window.location.pathname + window.location.search);
   }, []);
   return (
     <>
