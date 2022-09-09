3afe1a7e1473cdbac4b14dc7577cf639e253bf19
Pedro Arriagada <parriaga@alumnos.inf.utfsm.cl>
2022-09-09 03:11:38 -0300

    Cambiado el GA a UA

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Header from './components/HeaderFooter/Header';
 import Body from './components/Body/Body';
 import Footer from './components/HeaderFooter/Footer';
 import './App.css';
-import ReactGA from 'react-ga4';
-const TRACKING_ID = "G-2E4BMVMC2T"; // OUR_TRACKING_ID
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-238751221-1"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
   useEffect(() => {
-    ReactGA.send(window.location.pathname + window.location.search);
+    ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
   return (
     <>
